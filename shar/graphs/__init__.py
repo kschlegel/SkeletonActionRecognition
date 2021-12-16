dbea54f82bba56995147fa2873f4a967173a93fe
Kevin Schlegel <kevinschlegel42@gmail.com>
2021-12-16 21:32:03 +0000

    Add G3D graph convolution

--- shar/graphs/__init__.py
+++ shar/graphs/__init__.py
@@ -2,3 +2,4 @@ from .graph import Graph
 from .graphconvolution import GraphConvolution
 from .temporalgraphconvolution import TemporalGraphConvolution
 from .spatiotemporalgraphconvolution import SpatioTemporalGraphConvolution
+from .g3d import G3DGraphConvolution
